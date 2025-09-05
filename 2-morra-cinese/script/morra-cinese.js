c8021df44f692acbf43e5cda64f178dd5a1eafdb
LP Simeon <lp.simeon@yahoo.com>
2025-09-05 20:39:02 +0200

    Implemented initial version of "playRound" function

--- 2-morra-cinese/script/morra-cinese.js
+++ 2-morra-cinese/script/morra-cinese.js
@@ -2,6 +2,7 @@
 
 const choiceBtn = document.querySelector("#inizia");
 const choicesCtn = document.querySelector("#gioco #scelta");
+const cpuChoiceCtn = document.querySelector("#cpu");
 
 let playerPoints = document.querySelector("#punteggioYou");
 let cpuPoints = document.querySelector("#punteggioCpu");
@@ -16,3 +17,35 @@ const rock = document.querySelector("#sasso");
 choiceBtn.onclick = function () {
     choicesCtn.style.display = "block";
 };
+
+// Click events of player's choices
+paper.onclick = function () {
+    playRound("foglia");
+};
+
+scissors.onclick = function () {
+    playRound("forbice");
+};
+
+rock.onclick = function () {
+    playRound("sasso");
+};
+
+function playRound(playerChoice) {
+    console.log(playerChoice);
+    const cpuChoice = Math.floor(Math.random() * 3) + 1;
+
+    console.log(cpuChoice);
+
+    choicesCtn.style.display = "none";
+    document.querySelector("#you").style.backgroundImage =
+        "url('immagini/" + playerChoice + ".png')";
+
+    if (cpuChoice == 1) {
+        cpuChoiceCtn.style.backgroundImage = "url(' immagini/forbice.png')";
+    } else if (cpuChoice == 2) {
+        cpuChoiceCtn.style.backgroundImage = "url('immagini/sasso.png')";
+    } else if (cpuChoice == 3) {
+        cpuChoiceCtn.style.backgroundImage = "url('immagini/foglia.png')";
+    }
+}
