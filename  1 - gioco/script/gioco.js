d228e429b04b408f2ae0297199d26942adf24f91
LP Simeon <lp.simeon@yahoo.com>
2025-09-05 13:24:49 +0200

    Added "click" event for "#gioca" button

---  1 - gioco/script/gioco.js
+++  1 - gioco/script/gioco.js
@@ -1,3 +1,9 @@
 let cardYou = document.querySelector("#you > span");
 let cardCPU = document.querySelector("#cpu > span");
 let message = document.querySelector("#messaggio");
+const btnGioca = document.querySelector("#gioca");
+
+// Mouse click event of "GIOCA" button
+btnGioca.addEventListener("click", () => {
+  console.log("CIAO");
+});
