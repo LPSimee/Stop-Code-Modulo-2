d415858101208d446fb86f8091dedea1f1d803bb
LP Simeon <lp.simeon@yahoo.com>
2025-09-06 23:39:14 +0200

    Declared variables for the table, the endgame message and the reset button

--- 3-evita-la-bomba/js/script.js
+++ 3-evita-la-bomba/js/script.js
@@ -1,6 +1,9 @@
-document.addEventListener("DOMContentLoaded",function(){
+// For this game, we will use event listeners to handle user interactions.
 
+document.addEventListener("DOMContentLoaded", function () {
+    const table = document.querySelector("#gioco > ul");
+    const resultMessage = document.querySelector("#fine");
+    const newGameButton = document.querySelector("#new");
+}); //DOMContentLoaded
 
-   
-
-});//DOMContentLoaded
+// In this section we will declare the functions we will use in the game.
