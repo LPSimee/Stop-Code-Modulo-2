818428afdeb7287e3060e7773c94774bd99ad3dc
LP Simeon <lp.simeon@yahoo.com>
2025-09-07 10:32:05 +0200

    Added instruction txt file, added posRandomBmb and clickCounter variables

--- 3-evita-la-bomba/js/script.js
+++ 3-evita-la-bomba/js/script.js
@@ -1,5 +1,8 @@
 // For this game, we will use event listeners to handle user interactions.
 
+let posRandomBmb = Math.floor(Math.random() * 16); // For the bomb position, we generate a random number between 0 and 15 to match the index of the array.
+let clickCounter = 0; // This variable will count the number of safe clicks made by the player.
+
 document.addEventListener("DOMContentLoaded", function () {
     const table = document.querySelector("#gioco > ul");
     const resultMessage = document.querySelector("#fine");
