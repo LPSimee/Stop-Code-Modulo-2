471f13947d9c7380905811d0ded70683564374ed
LP Simeon <lp.simeon@yahoo.com>
2025-09-08 19:27:54 +0200

    Added "preventDefault" method on "submit" event of form

---
+++ 4-crea-la-tua-card/script/file.js
@@ -0,0 +1,11 @@
+// For the form validation I will use REGEX
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#modulo > form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+
+    });
+});
