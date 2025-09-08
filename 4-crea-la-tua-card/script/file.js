b6b85ea1f2ad112c099b2498b69836107aac32cd
LP Simeon <lp.simeon@yahoo.com>
2025-09-08 20:16:47 +0200

    Declared "checkInput" function

--- 4-crea-la-tua-card/script/file.js
+++ 4-crea-la-tua-card/script/file.js
@@ -2,10 +2,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("#modulo > form");
+    const inputName = document.querySelector("#nome");
+    const inputEmail = document.querySelector("#email");
+    const inputTel = document.querySelector("#tel");
+    const privacyCheckBox = document.querySelector("#privacy");
 
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-
+        if (checkName() && checkEmail() && checkTel() && privacyCheckBox.checked){
+            console.log("La tua card è stata creata con successo!");
+        }
     });
 });
+
+function checkInput(inputValue, regex) {
+    return regex.test(inputValue);
+}
