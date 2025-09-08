5365804b393c930df57bd7f5af7239ee7acca283
LP Simeon <lp.simeon@yahoo.com>
2025-09-08 20:25:32 +0200

    Added regex for input validation

--- 4-crea-la-tua-card/script/file.js
+++ 4-crea-la-tua-card/script/file.js
@@ -1,5 +1,11 @@
 // For the form validation I will use REGEX
 
+
+// Found these regex from the internet
+const regexName = /^([a-zA-Z0-9_\s]+)$/;
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regexTel = /^\+?(\d{1,3})?[-.\s]?(\(?\d{3}\)?[-.\s]?)?(\d[-.\s]?){6,9}\d$/;
+
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("#modulo > form");
     const inputName = document.querySelector("#nome");
