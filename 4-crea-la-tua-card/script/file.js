d55682ca66b6b19b2d8314e97f64cdc1f8d41137
LP Simeon <lp.simeon@yahoo.com>
2025-09-09 22:03:48 +0200

    Removed "DOMContentLoaded" event

--- 4-crea-la-tua-card/script/file.js
+++ 4-crea-la-tua-card/script/file.js
@@ -1,28 +1,26 @@
 // For the form validation I will use REGEX
 
-
 // Found these regex from the internet
 const regexName = /^([a-zA-Z0-9_\s]+)$/; // link: https://regexr.com/3agii
 const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // link: https://medium.com/@sketch.paintings/email-validation-with-javascript-regex-e1b40863ed23
 const regexTel = /^\+?(\d{1,3})?[-.\s]?(\(?\d{3}\)?[-.\s]?)?(\d[-.\s]?){6,9}\d$/; // link: https://www.reddit.com/r/GoogleForms/comments/1egqyxn/help_with_regex_validation_for_worldwide_email/?tl=it
 
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("#modulo > form");
-    const inputName = document.querySelector("#nome");
-    const inputEmail = document.querySelector("#email");
-    const inputTel = document.querySelector("#tel");
-    const privacyCheckBox = document.querySelector("#privacy");
+const inputMessages = ["La tua mail è sbagliata", "Telefono obbligatorio", "Devi accettare le condizioni di utilizzo"];
 
+const form = document.querySelector("#modulo > form");
+const inputName = document.querySelector("#nome");
+const inputEmail = document.querySelector("#email");
+const inputTel = document.querySelector("#tel");
+const privacyCheckBox = document.querySelector("#privacy");
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        console.log(checkInput(inputTel.value, regexTel));
+    console.log(checkInput(inputTel.value, regexTel));
 
-        if (checkInput(inputName.value, regexName) && checkInput(inputEmail.value, regexEmail) && checkInput(inputTel.value, regexTel) && privacyCheckBox.checked){
-            showCard(inputName.value, inputEmail.value, inputTel.value);
-        }
-    });
+    if (checkInput(inputName.value, regexName) && checkInput(inputEmail.value, regexEmail) && checkInput(inputTel.value, regexTel) && privacyCheckBox.checked){
+        showCard(inputName.value, inputEmail.value, inputTel.value);
+    }
 });
 
 function checkInput(inputValue, regex) {
